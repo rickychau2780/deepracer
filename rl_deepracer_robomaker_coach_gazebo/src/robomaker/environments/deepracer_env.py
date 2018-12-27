5b8cb3e2796006122ccbd44dfd1b70e39532067d
Ricky Chau <ricky.chau@digitaslbi.com>
2018-12-27 17:47:49 -0500

    Add steer penalty

--- rl_deepracer_robomaker_coach_gazebo/src/robomaker/environments/deepracer_env.py
+++ rl_deepracer_robomaker_coach_gazebo/src/robomaker/environments/deepracer_env.py
@@ -187,13 +187,27 @@ class DeepRacerEnv(gym.Env):
 
     def reward_function(self, on_track, x, y, distance_from_center, car_orientation, progress, steps,
                         throttle, steering, track_width, waypoints, closest_waypoints):
-        if distance_from_center >= 0.0 and distance_from_center <= 0.02:
-            return 1.0
-        elif distance_from_center >= 0.02 and distance_from_center <= 0.03:
-            return 0.3
-        elif distance_from_center >= 0.03 and distance_from_center <= 0.05:
-            return 0.1
-        return 1e-3  # like crashed
+        marker_1 = 0.1 * track_width
+        marker_2 = 0.25 * track_width
+        marker_3 = 0.5 * track_width
+
+        reward = 1e-3
+        if distance_from_center >= 0.0 and distance_from_center <= marker_1:
+            reward = 1
+        elif distance_from_center <= marker_2:
+            reward = 0.5
+        elif distance_from_center <= marker_3:
+            reward = 0.1
+        else:
+
+        # penalize reward for the car taking slow actions
+#         THROTTLE_THRESHOLD = 0.5
+#             if throttle < THROTTLE_THRESHOLD:
+#         reward *= 0.8            
+            
+        # add steering penalty
+            if abs(steering) > 0.5:
+        reward *= 0.80
 
     def infer_reward_state(self, steering_angle, throttle):
         # Wait till we have a image from the camera
